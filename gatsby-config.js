dc1f1cb556f41638a8b2bae73fb1b4aed8618fdf
alexdriaguine <alexdriagin12@gmail.com>
2019-12-17 18:40:02 +0100

    chore: fix syntax highlight

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,14 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              inlineCodeMarker: null,
+            },
+            aliases: {},
+          },
+          // `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
